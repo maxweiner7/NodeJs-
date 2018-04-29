6ce09494544cc0000e123e31c9cbca0dd8d5bbfd
unknown <max.wener@hotmail.com>
2018-04-29 18:32:03 +0100

    Comite 29/04/2018

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as Express from 'express';
-import UsuarioRota from './rotas/usuario';
-import PessoaRota from './rotas/pessoa';
-
-export function init(server: Express.Application) {
-  // INICIAR ROTAS
-  UsuarioRota(server);
-  PessoaRota(server);
-}
